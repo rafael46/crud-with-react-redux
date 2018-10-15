61961fb46da7007bb2a32bae93ece0af686dece7
Rafael Gdl <rafael@vipmarketingla.comcls>
2018-10-15 13:54:11 -0700

    setting up

--- src/index.js
+++ src/index.js
@@ -25,7 +25,10 @@ const reducer = combineReducers({
     authed: authReducer,
 })
 
-const store = createStore(reducer, applyMiddleware(ReduxThunk));
+const store = createStore(
+    reducer, applyMiddleware(ReduxThunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 
 
